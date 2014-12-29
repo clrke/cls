1257f8f56709e488eb6c2b2ce94927e0ffea89c3
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2014-12-29 22:04:18 +0800

    Add play game

--- play/urls.py
+++ play/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.home),
+	url(r'^user$', views.user),
+	url(r'^guest$', views.guest),
 )
--- play/views.py
+++ play/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User, AnonymousUser
 
 def home(request):
 	return render(request, 'play/index.html')
+
+@login_required(login_url= '/admin/login/')
+def user(request):
+	return render(request, 'play/game.html', {'user':request.user})
+
+def guest(request):
+	return render(request, 'play/game.html', {'user':AnonymousUser()})
