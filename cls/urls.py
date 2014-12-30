b2c5638273a7ced812e858be131bbeb3a2f2d2bb
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2014-12-30 18:34:03 +0800

    Redirect home to play

--- cls/urls.py
+++ cls/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.shortcuts import redirect
+
+def home(request):
+	return redirect('play.views.home')
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'cls.views.home', name='home'),
+    url(r'^$', home, name='home'),
     url(r'^play/', include('play.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
