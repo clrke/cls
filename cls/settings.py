8a99ab7635816ecbe989f2ffe38c2f3a6b1c781d
Clarke Benedict Tapay Plumo <clarke.plumo@gmail.com>
2014-12-30 22:45:17 +0800

    Fix database for production

--- cls/settings.py
+++ cls/settings.py
@@ -70,6 +70,10 @@ DATABASES = {
     }
 }
 
+if os.environ['DJANGO_ENVIRONMENT'] == 'production':
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
